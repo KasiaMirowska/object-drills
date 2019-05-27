cabed663dba2ea33c4c1e81c5571e916a5befb9a
Kasia <kasia@miroartdesign.com>
2019-05-27 12:05:51 -0400

    finished drills

--- drills.js
+++ drills.js
@@ -46,6 +46,8 @@ function deleteKey(obj) {
 
 console.log(deleteKey(myCats));
 
+
+
 function makeStudentsReport(data) {
   let studentArr = [];
 
@@ -55,8 +57,6 @@ function makeStudentsReport(data) {
   }
   return studentArr;
 }
-
-
 const testData = [
   { name: 'Jane Doe', grade: 'A' },
   { name: 'John Dough', grade: 'B' },
@@ -64,6 +64,59 @@ const testData = [
 ];
 
 console.log(makeStudentsReport(testData));
+
+
+
+
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' }
+];
+
+function findById(items, idNum) {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].id === idNum) {
+      return items[i];
+    }
+  }
+}
+console.log(findById(scratchData, 22));
+
+
+
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago'
+};
+
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria'
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  const newArr = Object.keys(object);
+
+  if (newArr.length !== expectedKeys.length) {
+    return false;
+  }
+
+  for (let i = 0; i < expectedKeys.length; i++) {
+    if (expectedKeys[i] !== newArr[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
 
 const loaf = {
   flour: 300,
@@ -74,6 +127,8 @@ const loaf = {
 }
 
 console.log(loaf.hydration())
+
+
 
 const obj1 = {
   foo: "hello",
@@ -97,9 +152,9 @@ keyCaller(obj1);
 const hobbitMeals = {
   meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
 }
-
-
 console.log(hobbitMeals.meals[3]);
+
+
 
 //ARRAY OF OBJECTS
 
@@ -177,4 +232,115 @@ function decipher(obj, str) {
 //   return deciphered;
 
 // }
-console.log(decipher(cipherObj, 'craft block argon meter bells brown croon droop'));
+const cipher = {
+    a: 2,
+    b: 3,
+    c: 4,
+    d: 5
+}
+
+const sentence = 'craft block argon meter bells brown croon droop'
+
+function decode( obj, str ) {
+    const strArr = str.split(' ');
+    const deciphered = strArr.map(word => {
+        let retStr;
+        if(obj[word[0]]) {
+            retStr = obj[word[0]]-1
+        }
+        return retStr ? word[retStr] : ' ';
+    });
+    return deciphered.join('');
+}
+
+decode( cipher, sentence );
+
+
+
+//Database Search:
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+
+function findOne(arr, obj){
+  let objId = Object.keys(obj).toString();
+  let objNum = Object.values(obj)[0];
+  
+  return arr.find(function(hero){
+    if (objId in hero){
+      if (hero[objId] === objNum) {
+        return hero;
+      }
+    }
+  });
+}
+console.log(findOne(HEROES, { id:3}));
+
+//factory functions
+
+function createCharacter(name,nickname,race, origin, attack, defense){
+  return {
+    name: name, // arr[0] vs. name
+    nickname: nickname,
+    race: race,
+    origin: origin,
+    attack: attack,
+    defense: defense,
+    describe: function() {
+      console.log(`${this.name} is a ${this.race} from ${this.origin}`);
+    },
+    evaluateFight: function(enemy){
+      let given;
+      let taken;
+      if (this.defense > enemy.attack){
+        taken = 0;
+      } else {
+        taken = Math.abs(this.defence - enemy.attack);
+      }
+      if (enemy.defense > this.attack) {
+        given = 0;
+      } else {
+        given = Math.abs(enemy.defence - this.attack);
+      }
+      console.log(`Your opponent takes ${given} damage and you receive ${taken} damage`);
+    }
+  };
+}
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'Aragorn', 'Man', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5),
+  createCharacter('Arwen Undomiel', 'arwen', 'half-elf', 'Rivendell', 22, 8)
+];
+console.log(characters);
+
+const findAragorn = characters.find(function(item){
+  return item.nickname === 'Aragorn';
+});
+findAragorn.describe();
+
+
+const findHobbits = characters.filter(function(item){
+  return item.race === 'Hobbit';
+});
+
+const fighters = characters.filter(function(item){
+  return item.attack > 5;
+});
+
+console.log(createCharacter('kasia','km','imaginary','poland',12,23));
+createCharacter('Jacob','Jake','Wizard','poland',12,23).evaluateFight({attack: 4, defence: 2});
+console.log(findAragorn);
+
+console.log(findHobbits);
+console.log(fighters);
